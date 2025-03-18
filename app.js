c7ef6c34503565c7c9a8f76c032bf0d45265cf3e
rafacallata <rcallata@mobuptacna.com>
2025-03-18 17:57:05 -0500

    add actualizarListaAmigos()

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ const ulListaAmigos = document.getElementById("listaAmigos");
 const ulResultado = document.getElementById("resultado");
 function agregarAmigo() {
     if (!inputAmigo.value) {
-        alert("Debes ingresar el nombre de tu amigo");
+        alert("Debes ingresar el nombre de tu amigo.");
+        return;
+    }
+    if(listaAmigos.includes(inputAmigo.value)){
+        alert(`El nombre ${inputAmigo.value} ya esta en la lista.`);
+        return;
     }
     const amigo = inputAmigo.value;
     listaAmigos.push(amigo);
-    ulListaAmigos.innerHTML += `<li>${amigo}</li>`;
-    console.log(listaAmigos);
     inputAmigo.value = "";
+
+    actualizarListaAmigos(amigo);
+}
+
+function actualizarListaAmigos(amigo) {
+    ulListaAmigos.innerHTML = "";
+    listaAmigos.forEach((amigo) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        ulListaAmigos.appendChild(li);
+    });
+    //ulListaAmigos.innerHTML += `<li>${amigo}</li>`;
 }
 
 function sortearAmigo() {
